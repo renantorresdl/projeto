af3685b8b99d26ac228a2832e39bc7b1b53f0e13
Renan Torres <75033049+renantorresdl@users.noreply.github.com>
2026-08-06 22:17:05 -0300

    Add files via upload

---
+++ jogo_jokenpo.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+
+    int escolhajogador, escolhaComputador;
+    srand(time(0));
+
+    printf("Jogo de Jokenpo\n");
+    printf("Escolha uma opção:\n");
+    printf("1. Pedra\n");
+    printf("2. Papel\n");
+    printf("3. Tersoura\n");
+    printf("Escolha: ");
+    scanf("%d", &escolhajogador);
+
+    escolhaComputador = rand() % 3 + 1;
+
+    switch (escolhajogador)
+    {
+    case 1:
+        printf("Jogador: Pedra - ");
+        break;
+        case 2:
+        printf("Jogador: Papel - ");
+        break;
+        case 3:
+        printf("Jogador: Tesoura - ");
+        break;
+    
+    default:
+    printf("Opção Inválida!\n");
+        break;
+    }
+
+        switch (escolhaComputador)
+    {
+    case 1:
+        printf("Computador: Pedra\n");
+        break;
+        case 2:
+        printf("Computador: Papel\n");
+        break;
+        case 3:
+        printf("Computador: Tesoura\n");
+        break;
+    }
+
+    if (escolhaComputador == escolhajogador)
+    {
+        printf("------ EMPATE ------");
+
+    } else if ((escolhajogador == 1) && (escolhaComputador == 3) ||
+                (escolhajogador == 2) && (escolhaComputador == 1) ||
+                (escolhajogador == 3) && (escolhaComputador == 2))
+    {
+        printf("------ VOCÊ GANHOU! ------");
+    } else {
+        printf("------ VOCÊ PERDEU! ------");
+    }
+    
+
+    return 0;
+}
